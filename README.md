9430e141b0c263a02553642a58e806ca2cad8812
Kristian Notari <kristiannotari@icloud.com>
2020-04-09 12:43:14 +0200

    initial commit migrated project goal

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
-# Elm Data Viewer
+# Elm Day Focus
 
 > Training project for learning and exploring the Elm language.
 
-Website where you can set some named variables (eg. x = 42, y = 33) and draw the selected graphs for them (plots, charts, etc.).
+Website where you can set some to-do items, label them and then keep track of what's next, with logged time spent on each.
 
 First step is to make the above description real by setting up a working website. Then, further features are likely to be added to the project.
--- src/Main.elm
+++ src/Main.elm
@@ -1,13 +1,6 @@
 module Main exposing (Model, Msg, init, subscriptions, update, view)
 
-import Array exposing (Array)
 import Browser
-import Dict exposing (Dict)
-import Html
-import Html.Attributes
-import Html.Events
-import Maybe.Extra
-import View.Plot as Plot
 
 
 
@@ -29,28 +22,11 @@ main =
 
 
 type alias Model =
-    { data : Array Data
-    , graphs : Dict String Bool
-    }
-
-
-type alias Data =
-    { name : String
-    , value : Float
-    , color : String
-    }
-
-
-type Figure
-    = Plot
+    {}
 
 
 type Msg
-    = ToggleGraphTypeCheck String
-    | AddVariableData
-    | ChangeVariableDataName Int Data String
-    | ChangeVariableDataValue Int Data String
-    | ChangeVariableDataColor Int Data String
+    = None
 
 
 
@@ -59,7 +35,7 @@ type Msg
 
 init : () -> ( Model, Cmd Msg )
 init _ =
-    ( { data = Array.fromList [ { name = "x", value = 80, color = "#ff2312" } ], graphs = Dict.fromList [ ( figureToString Plot, False ) ] }, Cmd.none )
+    ( {}, Cmd.none )
 
 
 
@@ -68,21 +44,7 @@ init _ =
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-    case msg of
-        ToggleGraphTypeCheck figure ->
-            ( { model | graphs = toggle figure model.graphs }, Cmd.none )
-
-        AddVariableData ->
-            ( { model | data = Array.push { name = "y", value = 15, color = "#551298" } model.data }, Cmd.none )
-
-        ChangeVariableDataName index data name ->
-            ( { model | data = Array.set index { data | name = name } model.data }, Cmd.none )
-
-        ChangeVariableDataValue index data value ->
-            ( { model | data = Array.set index { data | value = Maybe.withDefault 0 (String.toFloat value) } model.data }, Cmd.none )
-
-        ChangeVariableDataColor index data color ->
-            ( { model | data = Array.set index { data | color = color } model.data }, Cmd.none )
+    ( {}, Cmd.none )
 
 
 
@@ -100,126 +62,7 @@ subscriptions model =
 
 view : Model -> Browser.Document Msg
 view model =
-    { title = "Elm Data Viewer"
+    { title = "Elm Day Focus"
     , body =
-        [ viewHeader model
-        , viewMain model
-        ]
+        []
     }
-
-
-viewHeader : Model -> Html.Html Msg
-viewHeader model =
-    Html.header []
-        [ Html.h1 [] [ Html.text "Elm Data Viewer" ]
-        ]
-
-
-viewMain : Model -> Html.Html Msg
-viewMain model =
-    Html.main_ []
-        [ viewDataList model
-        , viewGraphTypesList model
-        , viewFiguresList model
-        ]
-
-
-viewDataList : Model -> Html.Html Msg
-viewDataList model =
-    Html.section [ Html.Attributes.id "values" ]
-        [ Html.header [] [ Html.text "Values" ]
-        , Html.p [] [ Html.text "Insert one or more values" ]
-        , Html.div [ Html.Attributes.id "values-wrapper" ] (List.map viewData (Array.toIndexedList model.data))
-        , Html.button [ Html.Events.onClick AddVariableData ] [ Html.text "Add" ]
-        ]
-
-
-viewData : ( Int, Data ) -> Html.Html Msg
-viewData ( index, data ) =
-    Html.div [ Html.Attributes.class "value-wrapper" ]
-        [ Html.input [ Html.Attributes.type_ "text", Html.Attributes.value data.name, Html.Events.onInput (\s -> ChangeVariableDataName index data s) ] []
-        , Html.input [ Html.Attributes.type_ "number", Html.Attributes.value (String.fromFloat data.value), Html.Events.onInput (\s -> ChangeVariableDataValue index data s) ] []
-        , Html.input [ Html.Attributes.type_ "text", Html.Attributes.value data.color, Html.Events.onInput (\s -> ChangeVariableDataColor index data s) ] []
-        , Html.div [ Html.Attributes.style "background-color" data.color ] []
-        ]
-
-
-viewGraphTypesList : Model -> Html.Html Msg
-viewGraphTypesList model =
-    Html.section [ Html.Attributes.id "graphs-selection" ]
-        [ Html.header [] [ Html.text "Graphs" ]
-        , Html.p [] [ Html.text "Select one or more graphs" ]
-        , Html.div [ Html.Attributes.id "graphs-selection-wrapper" ] <| List.map viewGraphType <| Dict.keys model.graphs
-        ]
-
-
-viewGraphType : String -> Html.Html Msg
-viewGraphType figure =
-    Html.div [ Html.Attributes.class "graph-checkbox-wrapper" ]
-        [ Html.input [ Html.Attributes.id <| "gs-" ++ figure, Html.Attributes.type_ "checkbox", Html.Events.onClick <| ToggleGraphTypeCheck figure ] []
-        , Html.label [ Html.Attributes.for <| "gs-" ++ figure ] [ Html.text figure ]
-        ]
-
-
-viewFiguresList : Model -> Html.Html Msg
-viewFiguresList model =
-    Html.section [ Html.Attributes.id "figures" ]
-        [ Html.header [] [ Html.text "Figures" ]
-        , Html.div [ Html.Attributes.id "figures-wrapper" ] <| Maybe.Extra.values <| List.map (viewFigure (Array.toList model.data)) (Dict.toList model.graphs)
-        ]
-
-
-viewFigure : List Data -> ( String, Bool ) -> Maybe (Html.Html Msg)
-viewFigure data ( figure, selected ) =
-    if not selected then
-        Nothing
-
-    else
-        Just
-            (Html.figure [] [ drawFigure data <| stringToFigure figure ])
-
-
-drawFigure : List Data -> Figure -> Html.Html Msg
-drawFigure data figure =
-    case figure of
-        Plot ->
-            Plot.draw { width = 300, height = 250 } <| toPlotData data
-
-
-
--- UTILS
-
-
-toPlotData : List Data -> List Plot.Variable
-toPlotData data =
-    List.map
-        (\d ->
-            { label = d.name
-            , value = d.value
-            , color = Just d.color
-            , scaledValue = Nothing
-            }
-        )
-        data
-
-
-toggle : String -> Dict String Bool -> Dict String Bool
-toggle figure dict =
-    Dict.update figure (\maybeOld -> Maybe.map not maybeOld) dict
-
-
-figureToString : Figure -> String
-figureToString figure =
-    case figure of
-        Plot ->
-            "plot"
-
-
-stringToFigure : String -> Figure
-stringToFigure figure =
-    case figure of
-        "Plot" ->
-            Plot
-
-        _ ->
-            Plot
