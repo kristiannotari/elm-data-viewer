1e949db6a79a57d22d0b4004eaa901817e80d13f
Kristian Notari <kristiannotari@icloud.com>
2020-04-14 19:00:42 +0200

    added card creation

--- src/Main.elm
+++ src/Main.elm
@@ -3,6 +3,8 @@ module Main exposing (Model, Msg, init, subscriptions, update, view)
 import Browser
 import Html exposing (Html)
 import Html.Attributes
+import Html.Events
+import String
 
 
 
@@ -23,7 +25,12 @@ main =
 -- MODEL
 
 
-type alias Model =
+type Model
+    = Normal State
+    | CardCreation State Card
+
+
+type alias State =
     { cards : List Card
     , labels : List Label
     }
@@ -44,7 +51,10 @@ type alias Label =
 
 
 type Msg
-    = Undefined
+    = CardCreationStarted
+    | CardCreationDone
+    | CardCreationDiscarded
+    | CardCreationChangedTitle String
 
 
 
@@ -53,7 +63,7 @@ type Msg
 
 init : () -> ( Model, Cmd Msg )
 init _ =
-    ( { cards = defaultCards, labels = defaultLabels }, Cmd.none )
+    ( Normal { cards = defaultCards, labels = defaultLabels }, Cmd.none )
 
 
 defaultCards : List Card
@@ -78,13 +88,36 @@ defaultLabels =
     ]
 
 
+defaultNewCard : Card
+defaultNewCard =
+    { title = ""
+    , spentTime = 0
+    , plannedTime = Nothing
+    , label = Nothing
+    }
+
+
 
 -- UPDATE
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-    ( model, Cmd.none )
+    case ( msg, model ) of
+        ( CardCreationStarted, Normal state ) ->
+            ( CardCreation state defaultNewCard, Cmd.none )
+
+        ( CardCreationDone, CardCreation state card ) ->
+            ( Normal { state | cards = List.append state.cards [ card ] }, Cmd.none )
+
+        ( CardCreationDiscarded, CardCreation state _ ) ->
+            ( Normal state, Cmd.none )
+
+        ( CardCreationChangedTitle title, CardCreation state card ) ->
+            ( CardCreation state { card | title = title }, Cmd.none )
+
+        _ ->
+            ( model, Cmd.none )
 
 
 
@@ -105,7 +138,8 @@ view model =
     { title = "Elm Day Focus"
     , body =
         [ viewHeaderBar
-        , viewCards model.cards
+        , viewCards model
+        , viewCardCreation model
         ]
     }
 
@@ -120,16 +154,35 @@ viewHeaderBar =
 
 viewMenuItems : List (Html Msg)
 viewMenuItems =
-    [ Html.img [ Html.Attributes.src "/assets/add-black-36dp.svg" ] []
+    [ Html.img [ Html.Events.onClick CardCreationStarted, Html.Attributes.src "/assets/add-black-36dp.svg" ] []
     , Html.img [ Html.Attributes.src "/assets/settings-black-36dp.svg" ] []
     ]
 
 
-viewCards : List Card -> Html Msg
-viewCards cards =
-    Html.section [] <| List.map viewCard cards
+viewCards : Model -> Html Msg
+viewCards model =
+    case model of
+        Normal state ->
+            Html.section [] <| List.map viewCard state.cards
+
+        CardCreation state _ ->
+            Html.section [] <| List.map viewCard state.cards
 
 
 viewCard : Card -> Html Msg
 viewCard card =
     Html.div [] [ Html.text card.title ]
+
+
+viewCardCreation : Model -> Html Msg
+viewCardCreation model =
+    case model of
+        Normal _ ->
+            Html.text ""
+
+        CardCreation _ card ->
+            Html.form [ Html.Events.onSubmit CardCreationDone ]
+                [ Html.input [ Html.Events.onInput (\value -> CardCreationChangedTitle value), Html.Attributes.placeholder "Card Title", Html.Attributes.autofocus True ] [ Html.text card.title ]
+                , Html.button [ Html.Attributes.disabled (String.isEmpty card.title), Html.Events.onClick CardCreationDone ] [ Html.text "Add" ]
+                , Html.button [ Html.Events.onClick CardCreationDiscarded ] [ Html.text "Cancel" ]
+                ]
